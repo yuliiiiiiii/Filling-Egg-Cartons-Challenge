c353226edf61ccb90ef0a1ae7c3a0212e8aa58ec
Yuli Wang <yuliwang1007@gmail.com>
2023-11-01 16:31:04 -0400

    create fillEggCartons function

--- fillEggCartons.js
+++ fillEggCartons.js
@@ -1,15 +1,36 @@
-import { random } from "./helper";
+const random = function() {
+  const prices = [25, 30, 35, 40, 45, 50];
+  const index = Math.floor(Math.random() * prices.length);
+  return prices[index];
+}
 
 function fillEggCartons(numEggs) {
   const objArr = [];
   // YOUR CODE
+    if (numEggs < 12) {
+      return "Egg number must be equal or larger than 12"
+    }
+
+    let allEggsArray = [];
+    for (let i=0; i<numEggs; i++) {
+      allEggsArray.push(random())
+    }
     
-  return objArr;
+    const fullCartons = Math.floor(numEggs / 12);
+    for (let i=0; i<fullCartons; i++) {
+      let acc = {eggs:[], price:0};
+
+      acc.eggs = allEggsArray.slice(i*12, (i+1)*12);
+      acc.price = acc.eggs.reduce((sum,curr) => sum+=curr);
+      objArr.push(acc);
+    }
+    
+  return objArr.sort((a,b) => a.price - b.price);
 }
 
-console.log(fillEggCartons(36));
+// console.log(fillEggCartons(36));
 console.log(fillEggCartons(52));
-console.log(fillEggCartons(152));
+// console.log(fillEggCartons(152));
 
 
 
--- helper.js
+++
@@ -1,7 +0,0 @@
-const random = function() {
-  const prices = [25, 30, 35, 40, 45, 50];
-  const index = math.floor(math.random() * prices.length);
-  return prices[index];
-}
-
-module.exports = { random };
