d79ba6677e2f07572b27cb65d37f529c0ca40487
Yuli Wang <yuliwang1007@gmail.com>
2023-11-01 17:26:15 -0400

    change the cheapest and expensive array

--- fillEggCartons.js
+++ fillEggCartons.js
@@ -8,7 +8,7 @@ function fillEggCartons(numEggs) {
   const objArr = [];
   // YOUR CODE
     if (numEggs < 12) {
-      return "Egg number must be equal or larger than 12"
+      return "Each carton must hold 12 eggs"
     }
 
     let allEggsArray = [];
@@ -25,12 +25,38 @@ function fillEggCartons(numEggs) {
       objArr.push(acc);
     }
     
-  return objArr.sort((a,b) => a.price - b.price);
+    // return objArr.sort((a,b) => a.price - b.price);
+// [
+// {eggs: Array(12), price: 430},
+// {eggs: Array(12), price: 430},
+// {eggs: Array(12), price: 435},
+// {eggs: Array(12), price: 445},
+// {eggs: Array(12), price: 450},
+// {eggs: Array(12), price: 455}, 
+// {eggs: Array(12), price: 460}, 
+// {eggs: Array(12), price: 460},
+// {eggs: Array(12), price: 470},
+// {eggs: Array(12), price: 480},
+// {eggs: Array(12), price: 500},
+// {eggs: Array(12), price: 505}
+// ]
+     let sortedObjArr = objArr.sort((a,b) => a.price - b.price);
+
+     let cheapestArray = sortedObjArr[0].eggs;
+     cheapestArray.includes(25) ? cheapestArray.splice(cheapestArray.indexOf(25), 1, 51) : console.log("No 25-cent egg in the cheapest carton") 
+     sortedObjArr[0].eggs = cheapestArray;
+     
+     
+     let expensiveArray = sortedObjArr[(sortedObjArr.length - 1)].eggs;
+     expensiveArray.includes(50) ? expensiveArray.splice(expensiveArray.indexOf(50),1,24) : console.log("No 50-cent egg in the most expensive carton") 
+     sortedObjArr[(sortedObjArr.length - 1)].eggs = expensiveArray;
+
+     
 }
 
 // console.log(fillEggCartons(36));
-console.log(fillEggCartons(52));
-// console.log(fillEggCartons(152));
+// console.log(fillEggCartons(52));
+ console.log(fillEggCartons(152));
 
 
 
